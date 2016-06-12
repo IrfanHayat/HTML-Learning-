ed8bae422909c37e2e20de43e42d586cbad6c248
IrfanHayat <irfanhayat55@gmail.com>
2016-06-12 12:36:28 +0500

    PHP GET

---
+++ LRU.php
@@ -0,0 +1,13 @@
+<?php
+	$page_number=0 ;$i=0;
+	echo "  <h1>  FIFO PAGE REPLACEMENT IMPLEMENTATION</h1><br><\n>";
+	print "Enter the number of page  <br>";  
+    $page_number= $_GET["page_number"];
+	echo "\n \t\t ENTER THE PAGE NUMBER:";
+	$str  = $_str1["str"];
+$pieces = explode(" ", $str);
+echo $pieces[0]; // piece1
+echo $pieces[1]; // piece2
+	
+	
+?>
